31378bd40e887f1c0d83086b052c1b47ae2aa406
vagrant <vagrant@ubuntu-vagrant>
2015-12-01 16:21:27 +0000

    Update

--- ksnctf.sweetduet.info/02.pl
+++ ksnctf.sweetduet.info/02.pl
@@ -3,25 +3,21 @@ use common::sense;
 my $original
     = 'EBG KVVV vf n fvzcyr yrggre fhofgvghgvba pvcure gung ercynprf n yrggre jvgu gur yrggre KVVV yrggref nsgre vg va gur nycunorg. EBG KVVV vf na rknzcyr bs gur Pnrfne pvcure, qrirybcrq va napvrag Ebzr. Synt vf SYNTFjmtkOWFNZdjkkNH. Vafreg na haqrefpber vzzrqvngryl nsgre SYNT';
 
-my $decrypted = lc $original;
-
-do {
-    say "$_ is " . chr($_);
-    }
-    for 97 .. 122;
+#do {
+#    say "$_ is " . chr($_);
+#    }
+#    for 1 .. 122;
 
 sub ceaser {
     my ($orig, $shift) = @_;
 
+    my ($A, $B) = map { chr(($shift + $_ + 26) % 26 + 65) } 0 .. 1;
     my ($a, $b) = map { chr(($shift + $_ + 26) % 26 + 97) } 0 .. 1;
 
-    eval "\$orig =~ tr/a-z/$b-za-$a/";
+    eval "\$orig =~ tr/A-Za-z/$B-ZA-${A}$b-za-$a/";
     say $@;
     say $orig;
 
 }
 
-do {
-    ceaser $decrypted, $_;
-    }
-    for 1 .. 27;
+ceaser $original, 12;
